f66ea59d16e92f853a3cbf55c17cbae304b56112
Fryxis <lennycoston@outlook.fr>
2025-12-15 14:38:16 +0000

    Ajout des fichiers TP01

---
+++ main.tf
@@ -0,0 +1,81 @@
+resource "google_compute_network" "vpc" {
+	name                    = var.network_name
+	auto_create_subnetworks = false
+	description             = "VPC for lab infrastructure"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+	name          = var.subnet_name
+	ip_cidr_range = var.subnet_cidr
+	region        = var.gcp_region
+	network       = google_compute_network.vpc.id
+}
+
+resource "google_service_account" "vm" {
+	account_id   = var.instance_sa_name
+	display_name = "Terraform demo VM"
+}
+
+resource "google_compute_address" "vm" {
+	name   = "${var.instance_name}-ip"
+	region = var.gcp_region
+}
+
+resource "google_compute_instance" "vm" {
+	name         = var.instance_name
+	machine_type = var.machine_type
+	zone         = var.gcp_zone
+	tags         = var.instance_tags
+
+	boot_disk {
+		initialize_params {
+			image = var.instance_image
+			size  = var.boot_disk_size_gb
+		}
+	}
+
+	network_interface {
+		subnetwork = google_compute_subnetwork.subnet.id
+
+		access_config {
+			nat_ip = google_compute_address.vm.address
+		}
+	}
+
+	service_account {
+		email  = google_service_account.vm.email
+		scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+	}
+
+	shielded_instance_config {
+		enable_secure_boot          = true
+		enable_vtpm                 = true
+		enable_integrity_monitoring = true
+	}
+}
+
+resource "random_id" "bucket_suffix" {
+	byte_length = 4
+}
+
+resource "google_storage_bucket" "assets" {
+	name                        = format("%s-%s", var.bucket_name_prefix, random_id.bucket_suffix.hex)
+	location                    = var.gcs_location
+	storage_class               = var.gcs_storage_class
+	force_destroy               = false
+	uniform_bucket_level_access = true
+
+	versioning {
+		enabled = true
+	}
+}
+
+output "instance_public_ip" {
+	value       = google_compute_address.vm.address
+	description = "External IP for the VM"
+}
+
+output "bucket_name" {
+	value       = google_storage_bucket.assets.name
+	description = "Name of the storage bucket"
+}
---
+++ variables.tf
@@ -0,0 +1,88 @@
+variable "gcp_project_id" {
+	type        = string
+	description = "ID du projet GCP cible"
+}
+
+variable "gcp_region" {
+	type        = string
+	description = "Region par defaut"
+	default     = "europe-west1"
+}
+
+variable "gcp_zone" {
+	type        = string
+	description = "Zone par defaut"
+	default     = "europe-west1-b"
+}
+
+variable "network_name" {
+	type        = string
+	description = "Nom du VPC"
+	default     = "lab-vpc"
+}
+
+variable "subnet_name" {
+	type        = string
+	description = "Nom du sous-reseau"
+	default     = "lab-subnet"
+}
+
+variable "subnet_cidr" {
+	type        = string
+	description = "Plage CIDR du sous-reseau"
+	default     = "10.10.0.0/24"
+}
+
+variable "instance_name" {
+	type        = string
+	description = "Nom de l'instance VM"
+	default     = "lab-vm"
+}
+
+variable "machine_type" {
+	type        = string
+	description = "Type de machine GCE"
+	default     = "e2-micro"
+}
+
+variable "boot_disk_size_gb" {
+	type        = number
+	description = "Taille du disque de boot en Go"
+	default     = 20
+}
+
+variable "instance_image" {
+	type        = string
+	description = "Image utilisee pour la VM"
+	default     = "projects/debian-cloud/global/images/family/debian-12"
+}
+
+variable "instance_tags" {
+	type        = list(string)
+	description = "Tags reseau appliques a la VM"
+	default     = ["http-server"]
+}
+
+variable "instance_sa_name" {
+	type        = string
+	description = "Identifiant du service account pour la VM"
+	default     = "tf-lab-vm"
+}
+
+variable "bucket_name_prefix" {
+	type        = string
+	description = "Prefixe du bucket GCS (suffixe aleatoire ajoute)"
+	default     = "tf-lab-bucket"
+}
+
+variable "gcs_location" {
+	type        = string
+	description = "Localisation du bucket GCS"
+	default     = "EU"
+}
+
+variable "gcs_storage_class" {
+	type        = string
+	description = "Classe de stockage du bucket"
+	default     = "STANDARD"
+}
