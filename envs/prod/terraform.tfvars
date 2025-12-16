883cc7221376bd92806932790a37df07cefe37e0
Fryxis <lennycoston@outlook.fr>
2025-12-16 10:57:18 +0000

    test

--- envs/prod/terraform.tfvars
+++ envs/prod/terraform.tfvars
@@ -1,10 +1,10 @@
-environment      = "prod"
-student_name     = "lenny"
-gcp_project_id   = "terraform-cours-481313"
-gcp_region       = "europe-west1"
-gcp_zone         = "europe-west1-b"
-machine_type     = "e2-micro"
-subnet_cidr      = "10.1.0.0/24"
+environment    = "prod"
+student_name   = "lenny"
+gcp_project_id = "terraform-cours-481313"
+gcp_region     = "europe-west1"
+gcp_zone       = "europe-west1-b"
+machine_type   = "e2-micro"
+subnet_cidr    = "10.1.0.0/24"
 
 # Liste des IPs pour les VMs
 vm_ips = [
--- provider.tf
+++ provider.tf
@@ -1,20 +1,20 @@
 terraform {
-	required_version = ">= 1.6"
+  required_version = ">= 1.6"
 
-	required_providers {
-		google = {
-			source  = "hashicorp/google"
-			version = ">= 5.0"
-		}
-		random = {
-			source  = "hashicorp/random"
-			version = ">= 3.6"
-		}
-	}
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6"
+    }
+  }
 }
 
 provider "google" {
-	project = var.gcp_project_id
-	region  = var.gcp_region
-	zone    = var.gcp_zone
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = var.gcp_zone
 }
